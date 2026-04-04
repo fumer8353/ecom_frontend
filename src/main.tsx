4ecc41d142531672f36aaa8c11199ab432ddf282
Umer Farooq <fumer8353@gmail.com>
2026-04-05 01:39:41 +0500

    3 commit

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,22 @@ import App from './App'
 import './style.css'
 import { AuthProvider } from './state/auth'
 import { CartProvider } from './state/cart'
+import { initApi } from './api'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+async function bootstrap() {
+  await initApi()
 
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+  )
+}
+
+bootstrap()
