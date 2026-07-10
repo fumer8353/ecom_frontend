99010bef093543aa512fc55a8f94f032be54aa5f
Umer Farooq <fumer8353@gmail.com>
2026-07-10 12:32:30 +0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ecom_frontend/',   // 👈 replace with your actual repo nam
   server: {
     port: 5173,
   },
